f5bf28d143848e32c6f9e22354da6272caac9961
Waseem Akram <waseepk@gmail.com>
2024-12-19 03:57:47 +0000

    Step 3: Added mail_sent hook and callback skeleton

--- pdf-generator.php
+++ pdf-generator.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: PDF Generator
  * Description: Generate a PDF after a specific Contact Form 7 form submission.
- * Version: 0.2
+ * Version: 0.3
  * Author: Waseem Akram
  */
 
@@ -10,5 +10,15 @@ if (!class_exists('FPDF')) {
     require_once plugin_dir_path(__FILE__) . 'fpdf/fpdf.php';
 }
 
-// The form title we are targeting
 define('TARGET_CF7_FORM_TITLE', 'Report');
+
+add_action('wpcf7_mail_sent', 'cf7_generate_pdf_after_submission_specific_form');
+function cf7_generate_pdf_after_submission_specific_form($contact_form) {
+    // Check the form title
+    $form_title = $contact_form->title();
+    if ($form_title !== TARGET_CF7_FORM_TITLE) {
+        return;
+    }
+
+    // The rest of the logic will come later
+}
