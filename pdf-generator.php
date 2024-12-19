f559aa5fed1e7a3ceda24e4597ac8258ef1e76ce
Waseem Akram <waseepk@gmail.com>
2024-12-19 03:56:48 +0000

    Step 2: Added FPDF requirement and form title constant

--- pdf-generator.php
+++ pdf-generator.php
@@ -2,6 +2,13 @@
 /**
  * Plugin Name: PDF Generator
  * Description: Generate a PDF after a specific Contact Form 7 form submission.
- * Version: 0.1
+ * Version: 0.2
  * Author: Waseem Akram
  */
+
+if (!class_exists('FPDF')) {
+    require_once plugin_dir_path(__FILE__) . 'fpdf/fpdf.php';
+}
+
+// The form title we are targeting
+define('TARGET_CF7_FORM_TITLE', 'Report');
